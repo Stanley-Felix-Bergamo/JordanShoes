05af6acee22f99fb2de6429f6849baeb3c15b0c1
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2023-02-16 13:33:44 -0300

    configuração do Gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,98 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
-        pkg: grunt.file.readJson('package.json'),
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    './dist/styles/main.min.css': './src/styles/main.less'
+                }
+            }
+        },
+
+        watch: {
+            less: {
+                files: {
+                    files: ['./src/styles/**/*.less'],
+                    tasks: ['less:development']
+                }
+            },
+            html: {
+                files: ['./src/index.html'],
+                tasks: ['replace:dev']
+            }
+        },
+
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'prebuild/index.min.html': 'src/index.html'
+                }
+            }
+        },
+
+        replace: {
+            development: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './dev/styles/main.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['./src/index.html'],
+                        dest: './dev'
+                    }
+                ]
+            },
+
+            production: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './dist/styles/main.min.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.min.html'],
+                        dest: './dist'
+                    }
+                ]
+            }
+        },
+        clean: ['prebuild']
     });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less', 'htmlmin', 'replace','clean']);
 }
+
